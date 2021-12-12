ac11f11712d3a43eccfe4de9fa9f7db7fce51dc8
danyy <dany@gmail.com>
2021-12-11 18:02:05 -0600

    Modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import NuevoPresupuesto from "./NuevoPresupuesto"
+import ControlPresupuesto from "./ControlPresupuesto";
+import NuevoPresupuesto from "./NuevoPresupuesto";
 
 const Header = ({
     presupuesto,
@@ -11,7 +12,9 @@ const Header = ({
             <h1>Planificador de Gastos</h1>
 
             {isValidPresupuesto ? (
-                <p>Control Presupuesto</p>
+                <ControlPresupuesto 
+                    presupuesto={presupuesto}
+                />
             ) : (
                 <NuevoPresupuesto
                     presupuesto={presupuesto}
